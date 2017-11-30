afc503936d40105de7224ef904f23ec4ae7f6799
Philippe Kirsch <philippe.kirsch@hotmail.fr>
2017-11-30 22:12:56 +0100

    new model

--- models.py
+++ models.py
@@ -7,40 +7,67 @@ import torchvision.models as M
 # self.fc0 = nn.Linear(28*28, 27)
 # x = self.c0(x.view(x.size(0), -1))
 class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.c0 = nn.Conv2d(1, 16, 1*1, stride=1, padding=0, dilation=1, groups=1, bias=True)
-        self.fc0 = nn.ReLU(inplace=False)
+	def __init__(self):
+		super(Net, self).__init__()
+		self.c0 = nn.Conv2d(1, 16, 1*1, stride=1, padding=0, dilation=1, groups=1, bias=True)
+		self.fc0 = nn.ReLU(inplace=False)
 
-    def forward(self, x):
-    	print(x.size())
-        x = self.c0(x)
-        x = self.fc0(x)
-        
-        return F.log_softmax(x)
+	def forward(self, x):
+		print(x.size())
+		x = self.c0(x)
+		x = self.fc0(x)
+		
+		return F.log_softmax(x)
 
 		# self.layer1 = nn.Sequential()
 class Net(nn.Module):
 	def __init__(self):
 		super(Net, self).__init__()
 		self.layer1 = nn.Sequential(
-            nn.Conv2d(1, 20, 5),
-            nn.BatchNorm2d(20),
-            nn.MaxPool2d(2),
-            nn.ReLU())
+			nn.Conv2d(1, 20, 5),
+			# nn.BatchNorm2d(20),
+			nn.ReLU(),
+			nn.MaxPool2d(2)
+			)
 		self.layer2 = nn.Sequential(
 			nn.Conv2d(20, 50, 5),
-			nn.BatchNorm2d(50),
-			nn.MaxPool2d(2),
-			nn.ReLU())
+			# nn.BatchNorm2d(50),
+			nn.ReLU(),
+			nn.MaxPool2d(2)
+			)
 		self.fc1 = nn.Linear(4*4*50, 500)
 		self.fc2 = nn.Linear(500, 27)
 		self.ceriation = nn.CrossEntropyLoss()
-	    
+		
 	def forward(self, x):
 		x = self.layer1(x)
 		x = self.layer2(x)
 		x = x.view(-1, 4*4*50)
+		x = self.fc1(x)
+		x = self.fc2(x)
+		return x
+
+class Net(nn.Module):
+	def __init__(self):
+		super(Net, self).__init__()
+		self.layer1 = nn.Sequential(
+			nn.Conv2d(1, 32, kernel_size=(5, 5), stride=1, padding=2),
+			nn.ReLU(),
+			nn.MaxPool2d((2,2), stride=2, padding=0)
+			)
+		self.layer2 = nn.Sequential(
+			nn.Conv2d(32, 64, kernel_size=(5, 5), stride=1, padding=2),
+			nn.ReLU(),
+			nn.MaxPool2d((2,2), stride=2, padding=0)
+			)
+		self.fc1 = nn.Linear(7*7*64, 1024)
+		self.fc2 = nn.Linear(1024, 27)
+		self.ceriation = nn.CrossEntropyLoss()
+		
+	def forward(self, x):
+		x = self.layer1(x)
+		x = self.layer2(x)
+		x = x.view(-1, 7*7*64)
 		x = self.fc1(x)
 		x = self.fc2(x)
 		return x
